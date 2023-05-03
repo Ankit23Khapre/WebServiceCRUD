e8d338a79f4e147581a2646a4efe02c3980edcc4
Ankit23Khapre <ankit23khapre@gmail.com>
2023-05-03 17:14:58 +0530

    files added

---
+++ webservicecrudapp/src/main/java/com/example/demo/WebservicecrudappApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicecrudappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("AppRuning....");
+		SpringApplication.run(WebservicecrudappApplication.class, args);
+	}
+
+}
